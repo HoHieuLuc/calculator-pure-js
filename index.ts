c9d1b2b07321de6e1c6105101eac0c147bc20a47
Ho Hieu Luc <kingofika123@gmail.com>
2022-08-10 13:26:44 +0700

    good enough

--- index.ts
+++ index.ts
@@ -1,4 +1,29 @@
-const allButtons = [
+type ButtonType = 'number' | 'operator' | 'special';
+
+interface BaseButton {
+    type: ButtonType;
+}
+
+interface NumberButton extends BaseButton {
+    type: 'number';
+    label: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0';
+}
+
+interface OperatorButton extends BaseButton {
+    type: 'operator';
+    label: '+' | '-' | '*' | '/' | '.';
+}
+
+interface SpecialButton extends BaseButton {
+    type: 'special';
+    label: '=';
+}
+
+type Button = NumberButton | OperatorButton | SpecialButton;
+
+type Buttons = Array<Button>;
+
+const allButtons: Array<Buttons> = [
     [
         {
             label: '7',
@@ -73,14 +98,15 @@ const allButtons = [
     ]
 ];
 
-const buttonContainerDOM = document.querySelector('.button-container');
-const renderButton = (data) => {
+const buttonContainerDOM = document.querySelector('.button-container')!;
+
+const renderButton = (data: Array<Buttons>) => {
     buttonContainerDOM.innerHTML = `
         <div class="button-wrapper">
             <button class="button-long" onclick="clearAll()">C</button>
             <button class="button-long" onclick="deleteLast()">CE</button>
         </div>
-    ` + data.map((row) => {
+    `+ data.map((row) => {
         return `
             <div class="button-wrapper">
                 ${row.map((button) => `
@@ -99,25 +125,25 @@ renderButton(allButtons);
 
 const deleteLast = () => {
     isEqualButtonPreviouslyClicked = false;
-    if (currentResultDOM.textContent.toLowerCase().includes('error')) {
+    if (currentResultDOM.textContent!.toLowerCase().includes('error')) {
         currentResultDOM.textContent = '';
         return;
     }
-    currentResultDOM.textContent = currentResultDOM.textContent.slice(0, -1);
-};
+    currentResultDOM.textContent = currentResultDOM.textContent!.slice(0, -1);
+}
 
 const clearAll = () => {
     isEqualButtonPreviouslyClicked = false;
     currentResultDOM.textContent = '';
     prevExpressionDOM.textContent = '';
-};
+}
 
-const prevExpressionDOM = document.querySelector('#prev-expression');
-const currentResultDOM = document.querySelector('#result');
+const prevExpressionDOM = document.querySelector('#prev-expression')!;
+const currentResultDOM = document.querySelector('#result')!;
 
 let isEqualButtonPreviouslyClicked = false;
 
-const onButtonClick = (type, label) => {
+const onButtonClick = (type: Button['type'], label: Button['label']) => {
     // if the equal button is previously clicked
     // and the clicked button's type is "number"
     // replace the current result
@@ -131,16 +157,14 @@ const onButtonClick = (type, label) => {
         if (type === 'special') {
             prevExpressionDOM.textContent = currentResultDOM.textContent;
             /* the expression doesn't contain letter => valid */
-            if (currentResultDOM.textContent.match(/[a-zA-Z]/g) === null) {
-                const result = eval(currentResultDOM.textContent);
+            if (currentResultDOM.textContent!.match(/[a-zA-Z]/g) === null) {
+                const result = eval(currentResultDOM.textContent!);
                 if (result === Infinity || result === -Infinity) {
                     currentResultDOM.textContent = 'Overflow error';
-                }
-                else {
+                } else {
                     currentResultDOM.textContent = `${Math.round(result * 100) / 100}`;
                 }
-            }
-            else {
+            } else {
                 currentResultDOM.textContent = 'Syntax error';
             }
             isEqualButtonPreviouslyClicked = true;
@@ -148,14 +172,16 @@ const onButtonClick = (type, label) => {
         }
         // other buttons are clicked
         isEqualButtonPreviouslyClicked = false;
-        if (currentResultDOM.textContent.toLowerCase().includes('error')) {
+        if (currentResultDOM.textContent!.toLowerCase().includes('error')) {
             currentResultDOM.textContent = '';
         }
-        if (currentResultDOM.textContent.length < 21) {
+        if (currentResultDOM.textContent!.length < 21) {
             currentResultDOM.textContent += label;
         }
-    }
-    catch (e) {
+    } catch (e) {
         currentResultDOM.textContent = 'Syntax error';
     }
-};
+}
+// tsc .\temp.ts -t es2022 
+// xóa dòng export{} này bên file temp.js sau khi compile
+export { }
